551480718e275e86f9650f8dfd5957c983ea7558
Mitheel Ramdaw <106301219+mitheelramdaw@users.noreply.github.com>
2025-07-07 20:23:51 +0200

    Update app title and add new logo image
    Changed the app page title from 'Centre of Excellence' to 'PipStop' and added a new logo image at assets/logov2.png.

--- PipStop/PipStop.py
+++ PipStop/PipStop.py
@@ -2,4 +2,4 @@ import reflex as rx
 from PipStop.pages.home import home
 
 app = rx.App()
-app.add_page(home, route="/", title="Centre of Excellence")
+app.add_page(home, route="/", title="PipStop")
